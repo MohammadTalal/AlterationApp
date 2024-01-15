8007d94c39e0ea701a11e1c60873b89ae130c0a5
tshamout <tshamout@rosaflora.com>
2024-01-15 10:30:46 -0500

    update #2 - icons Jan 15

--- src/main.js
+++ src/main.js
@@ -5,9 +5,18 @@ import router from './router'
 // global styles
 import './assets/main.css'
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+
+library.add(fas)
+
 
 // Initialize Vue app
 const app = createApp(App);
+
+app.component('font-awesome-icon', FontAwesomeIcon)
 
 
 // Use router
